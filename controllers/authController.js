31e3e81d69260ec95e2c5c78bb879731e59af563
Kopyl10 <kopyl10@gmail.com>
2025-09-22 12:31:36 -0700

    feat(auth): add login controller (JWT sign + token save)

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
+const { JWT_SECRET = "dev-secret", JWT_EXPIRES_IN = "24h" } = process.env;
+
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user)
+    return res.status(401).json({ message: "Email or password is wrong" });
+
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok)
+    return res.status(401).json({ message: "Email or password is wrong" });
+
+  const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN,
+  });
+  user.token = token;
+  await user.save();
+
+  return res.status(200).json({
+    token,
+    user: { email: user.email, subscription: user.subscription },
+  });
+}

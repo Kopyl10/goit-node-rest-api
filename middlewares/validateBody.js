43ea64c50fbbc3144b12c1427760c358acb58895
Kopyl10 <kopyl10@gmail.com>
2025-09-22 12:12:21 -0700

    addition

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,6 +1,12 @@
 // middlewares/validateBody.js
 module.exports = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body);
-  if (error) return res.status(400).json({ message: error.message });
+  const { error } = schema.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(400).json({
+      message:
+        error.details?.[0]?.message ||
+        "Помилка від Joi або іншої бібліотеки валідації",
+    });
+  }
   next();
 };

a6fe4f210ab0cd9a60cd4f2d66f8b3ecc8b94138
Kopyl10 <kopyl10@gmail.com>
2025-09-23 11:56:55 -0700

    fix(auth): export loginSchema from authSchemas

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -4,8 +4,10 @@ const registerSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
+
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
-module.exports = { registerSchema };
+
+module.exports = { registerSchema, loginSchema };

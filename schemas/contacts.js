c46b48764e01bc74690975f338efeb17eaa1c14d
Kopyl10 <kopyl10@gmail.com>
2025-09-24 12:06:14 -0700

    Fixes 12

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,21 +2,24 @@
 const Joi = require("joi");
 
 const createContactSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(1).required(),
   email: Joi.string().email(),
-  phone: Joi.string(),
+  phone: Joi.string().min(3),
   favorite: Joi.boolean(),
 });
 
 const updateContactSchema = Joi.object({
-  name: Joi.string(),
+  name: Joi.string().min(1),
   email: Joi.string().email(),
-  phone: Joi.string(),
+  phone: Joi.string().min(3),
   favorite: Joi.boolean(),
 }).min(1);
 
 const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean().required().messages({
+    "any.required": "missing field favorite",
+    "boolean.base": "missing field favorite",
+  }),
 });
 
 module.exports = {
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,30 +1,39 @@
 // services/contactServices.js
-import Contact from "../models/contact.js";
+const Contact = require("../models/contact");
 
-export async function getAllContacts({ owner }) {
+async function getAllContacts({ owner }) {
   return Contact.find({ owner });
 }
 
-export async function getContactById(id, owner) {
+async function getContactById(id, owner) {
   return Contact.findOne({ _id: id, owner });
 }
 
-export async function createContact(data) {
+async function createContact(data) {
   return Contact.create(data);
 }
 
-export async function deleteContact(id, owner) {
+async function deleteContact(id, owner) {
   return Contact.findOneAndDelete({ _id: id, owner });
 }
 
-export async function updateContact(id, body, owner) {
+async function updateContact(id, body, owner) {
   return Contact.findOneAndUpdate({ _id: id, owner }, body, { new: true });
 }
 
-export async function updateStatusContact(id, favorite, owner) {
+async function updateStatusContact(id, favorite, owner) {
   return Contact.findOneAndUpdate(
     { _id: id, owner },
     { favorite },
     { new: true }
   );
 }
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  deleteContact,
+  updateContact,
+  updateStatusContact,
+};
